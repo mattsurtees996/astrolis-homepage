3948e08d2cc62a97c562b436eb7bd88d3df3dc53
mattsurtees996 <matthew.surtees@icloud.com>
2023-01-18 22:53:52 +0000

    add new js

--- app.js
+++ app.js
@@ -1,42 +1,47 @@
 let myName = prompt("What is your name?");
 
 let time = prompt("What hour of the day is it? (0-23)");
-let message;
+let greeting;
 
 if(time <= 11) {
-    message = "Good Morning " + myName + "!";
+    greeting = "Good Morning " + myName + "!";
 } else if (time <= 18) {
-    message = "Good Afternoon " + myName + "!"
+    greeting = "Good Afternoon " + myName + "!"
 } else if (time < 24) {
-    message = "Good Evening " + myName + "!"
+    greeting = "Good Evening " + myName + "!"
 } else {
-    message = "That is not a valid answer, please try again"
+    greeting = "That is not a valid answer, please try again"
 }
 
 
 let feedback1 = prompt("Where did you hear about us?")
 
-if (feedback1 == "Facebook") {
+let message; 
+
+if (feedback1.toLowerCase() == "facebook") {
     message = "Thank you for your choosing Facebook!";
-} else if (feedback1 == "Instagram") {
+} else if (feedback1.toLowerCase() == "instagram") {
     message = "Thank you for choosing Instagram!";
-} else if (feedback1 == "Twitter") {
+} else if (feedback1.toLowerCase() == "twitter") {
     message = "Thank you for choosing Twitter!";
-} else if (feedback1 == "Autotrader") {
+} else if (feedback1.toLowerCase() == "autotrader") {
     message = "Thank you for your feedback!";
 }
+
 
 let text;
 if (confirm("Accept Cookies?") == true) {
   text = "Accepted";
 } else {
-  text = "Nevermind.";
+  text = "Nevermind! Try Again" + location.reload();
 }
 console.log(message);
 
 document.write(message);
 
 console.log(text);
+
+
 
 
 
